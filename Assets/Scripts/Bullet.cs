9ad79ae0e495981d4142a43f40fa4dd784bf335f
dangkhoa <ddn38@drexel.edu>
2023-03-01 13:24:24 -0500

    added enemy sprite

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,3 +1,46 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class Bullet : MonoBehaviour
+// {
+//     public GameObject target;
+//     public float speed;
+//     public Rigidbody2D bulletRB;
+    // public Transform bulletSpawnPoint;
+//     public GameObject bullet;
+//     public float life;
+//     public float delay = 3;
+//     float timer;
+
+//     // Start is called before the first frame update
+ 
+
+//     void Update()
+//     {
+
+//         bullet = Instantiate(bullet, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+//         bulletRB = bullet.GetComponent<Rigidbody2D>();
+        
+//         target = GameObject.FindGameObjectWithTag("Player");
+
+//         Vector2 moveDir = (target.transform.position - transform.position).normalized * speed;
+//         bulletRB.velocity = new Vector2(moveDir.x, moveDir.y);
+//         Destroy(bullet, life);
+    
+
+
+//         //StartCoroutine(WaitForFunction());
+//     }
+    
+//     IEnumerator WaitForFunction()
+//     {
+//         yield return new WaitForSeconds(3);
+//         Debug.Log("wait");
+
+//     }
+// }
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +48,19 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     GameObject target;
+    public GameObject bullet;
     public float speed;
     Rigidbody2D bulletRB;
+    public Transform bulletSpawnPoint;
     // Start is called before the first frame update
     void Start()
     {
-        bulletRB = GetComponent<Rigidbody2D>();
+        //bulletRB = GetComponent<Rigidbody2D>();
+        bullet = Instantiate(bullet, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        bulletRB = bullet.GetComponent<Rigidbody2D>();
         target = GameObject.FindGameObjectWithTag("Player");
         Vector2 moveDir = (target.transform.position - transform.position).normalized * speed;
         bulletRB.velocity = new Vector2(moveDir.x, moveDir.y);
-        Destroy(this.gameObject, 2);
+        Destroy(bullet, 2);
     }
 }
--- Assets/Scripts/EnemyFollowPlayer.cs
+++ Assets/Scripts/EnemyFollowPlayer.cs
@@ -12,10 +12,16 @@ public class EnemyFollowPlayer : MonoBehaviour
     public GameObject bullet;
     public GameObject bulletParent;
     private Transform player;
+    public SpriteRenderer spriteRenderer;
+    Animator m_Animator;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        m_Animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,19 +31,30 @@ public class EnemyFollowPlayer : MonoBehaviour
         if(distanceFromPlayer<lineOfSight && distanceFromPlayer > shootingRange)
         {
             transform.position = Vector2.MoveTowards(this.transform.position, player.position, speed * Time.deltaTime);
+            m_Animator.SetBool("walking", true);
         }
         else if (distanceFromPlayer <= shootingRange && nextFireTime < Time.time)
         {
             Instantiate(bullet, bulletParent.transform.position, Quaternion.identity);
             nextFireTime = Time.time + fireRate;
+            m_Animator.SetBool("walking", false);
         }
+        Flip();
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(transform.position, lineOfSight);
-                Gizmos.DrawWireSphere(transform.position, shootingRange);
+        Gizmos.DrawWireSphere(transform.position, shootingRange);
 
     } 
+
+    void Flip() {
+        if(player.position.x - transform.position.x < 0) {
+            spriteRenderer.flipX = true;
+        } else if (player.position.x - transform.position.x > 0) {
+            spriteRenderer.flipX = false;
+        }
+    }
 }
