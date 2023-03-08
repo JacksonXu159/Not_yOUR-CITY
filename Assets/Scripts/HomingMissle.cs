61ab6f72659914907f4472cf475baa6c422c4bb0
Jackson <jacksonxu159@gmail.com>
2023-03-08 11:32:17 -0500

    added boss

---
+++ Assets/Scripts/HomingMissle.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class HomingMissle : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform target;
+    public float speed = 5f;
+    public float rotateSpeed = 200f;
+    private Rigidbody2D rb;
+    public GameObject explosionEffect;
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 direction = (Vector2)target.position - rb.position;
+
+        direction.Normalize();
+
+        float rotateAmount = Vector3.Cross(direction, transform.up).z;
+
+        rb.angularVelocity = -rotateAmount * rotateSpeed;
+
+        rb.velocity = transform.up * speed;
+    }
+
+    void OnTriggerEnter2D()
+    {
+        if (target.tag == "Player")
+        {
+            Instantiate(explosionEffect, transform.position, transform.rotation);
+            Destroy(gameObject);
+            
+        }
+
+    }
+}
