72da85b5d16d7cfc50c6ef152ae6cf26b1ddd18c
KevinIsMyName <wu.kevin.2520@gmail.com>
2023-03-15 15:23:33 -0400

    Make weapon rotation slightly smoother

--- Assets/Scripts/WeaponRotation.cs
+++ Assets/Scripts/WeaponRotation.cs
@@ -8,10 +8,10 @@ public class WeaponRotation : MonoBehaviour
     public SpriteRenderer characterRenderer;
     public SpriteRenderer weaponRenderer;
     private float yScale;
-    public float offset;
-    public float xOffset;
     public float yOffset;
     public Transform plr;
+
+    private int armLength = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,22 +25,28 @@ public class WeaponRotation : MonoBehaviour
         Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         difference.Normalize();
         float rotation_z = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, rotation_z + offset);
+        transform.rotation = Quaternion.Euler(0f, 0f, rotation_z);
 
         Vector2 scale = transform.localScale;
 
+        // Place weapon a certain distance from current player, based on mousePosition's difference
+        transform.position = new Vector3(
+            plr.position.x  + this.armLength * difference.x,
+            plr.position.y + this.armLength * difference.y,
+            plr.position.z + this.armLength * difference.z
+        );
 
         if(Mathf.Abs(rotation_z) > 90)
         {
             scale.y = -yScale;
             characterRenderer.flipX = true;
-            transform.position = new Vector3(plr.position.x - xOffset, plr.position.y+yOffset, plr.position.z);
-        }else if(Mathf.Abs(rotation_z) < 90)
+            transform.position = new Vector3(transform.position.x, transform.position.y+yOffset, transform.position.z);
+        }
+        else if(Mathf.Abs(rotation_z) < 90)
         {
             scale.y = yScale;
             characterRenderer.flipX = false;
-            transform.position = new Vector3(plr.position.x + xOffset, plr.position.y+yOffset, plr.position.z);
-
+            transform.position = new Vector3(transform.position.x, transform.position.y+yOffset, transform.position.z);
         }
         transform.localScale = scale;
     }
