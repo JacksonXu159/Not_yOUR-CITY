df8ce1f0a06ab70ab1851aec735ad77078587e88
dangkhoa <ddn38@drexel.edu>
2023-03-15 14:12:01 -0400

    boss stages, jx

--- Assets/Scripts/PlayerBullet.cs
+++ Assets/Scripts/PlayerBullet.cs
@@ -23,7 +23,7 @@ public class PlayerBullet : MonoBehaviour
 
         if (collision.gameObject.tag == "Guiliani")
         {
-            collision.gameObject.GetComponent<BossController>().health -= 2;
+            collision.gameObject.GetComponent<BossController>().health -= 5;
             Destroy(gameObject);
         }
     }
