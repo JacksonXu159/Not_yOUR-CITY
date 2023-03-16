5764321b3401f0035791c34d425660b26d3a83d4
KevinIsMyName <wu.kevin.2520@gmail.com>
2023-03-16 15:32:21 -0400

    Add gun shoot sound

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -10,18 +10,28 @@ public class Gun : MonoBehaviour
     public float nextFireTime;
     public GameObject player;
     float ammo;
+    private AudioSource audioOutputSource;
+    public AudioClip shootClip;
 
+    void Start()
+    {
+        audioOutputSource = gameObject.AddComponent<AudioSource>();
+    }
 
     void Update()
     {
         // Debug.Log(player.GetComponent<PlayerController>().inventory.CanShoot());
 
-        if(((Input.GetMouseButtonDown(0) && nextFireTime < Time.time)) && player.GetComponent<PlayerController>().inventory.CanShoot())
+        if(Input.GetMouseButtonDown(0) && nextFireTime < Time.time)
         {
-            player.GetComponent<PlayerController>().inventory.Shoot(1);
-            nextFireTime = Time.time + 1f;
-            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-            bullet.GetComponent<Rigidbody2D>().velocity = bulletSpawnPoint.right * bulletSpeed;
+            if (player.GetComponent<PlayerController>().inventory.CanShoot())
+            {
+                player.GetComponent<PlayerController>().inventory.Shoot(1);
+                nextFireTime = Time.time + 1f;
+                var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+                bullet.GetComponent<Rigidbody2D>().velocity = bulletSpawnPoint.right * bulletSpeed;
+                audioOutputSource.PlayOneShot(shootClip);
+            }
         }
     }
 }
