d0cd26e45e505850e35e3ee1e4513d589821b508
dangkhoa <ddn38@drexel.edu>
2023-03-20 13:17:11 -0400

    fixed missile explosion audio(untested)

--- Assets/Scripts/CloneDelete.cs
+++ Assets/Scripts/CloneDelete.cs
@@ -5,11 +5,6 @@ using UnityEngine;
 public class CloneDelete : MonoBehaviour
 {
     public int life = 1;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     void Awake(){
         Destroy(gameObject, life);
---
+++ Assets/Scripts/ExplosionCloneDelete.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionCloneDelete : MonoBehaviour
+{
+    public int life = 1;
+    private AudioSource audioOutputSource;
+    public AudioClip missileExplodeClip;
+    // Start is called before the first frame update
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Destroy(gameObject, life);
+
+    }
+
+    void Start()
+    {
+        audioOutputSource = gameObject.AddComponent<AudioSource>();
+        audioOutputSource.PlayOneShot(missileExplodeClip);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
